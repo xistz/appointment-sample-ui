d24470ace7cc44ec931a29b3253d7635e605da4d
Jonathan Chan <tmxistz@gmail.com>
2020-10-07 18:05:57 +0900

    added routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
 
-function App() {
+import {
+  Home,
+  Header,
+  Profile,
+  Register,
+  Loading,
+  PrivateRoute,
+  NotFound,
+} from './components';
+
+export default function App() {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Header />
+
+      {/* A <Switch> looks through its children <Route>s and
+renders the first one that matches the current URL. */}
+      <Switch>
+        <PrivateRoute exact path="/" component={Home} />
+        <PrivateRoute exact path="/profile" component={Profile} />
+        <PrivateRoute exact path="/register" component={Register} />
+        <Route component={NotFound} />
+      </Switch>
+    </React.Fragment>
   );
 }
-
-export default App;
