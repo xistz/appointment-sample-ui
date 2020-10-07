e418d0e7a85e7b191bb3ce865fc1ec90f6c412fd
Jonathan Chan <tmxistz@gmail.com>
2020-10-07 18:04:55 +0900

    added private route

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import { withAuthenticationRequired } from '@auth0/auth0-react';
+import Loading from './Loading';
+
+export default function PrivateRoute({ component, ...args }) {
+  return (
+    <Route
+      component={withAuthenticationRequired(component, {
+        onRedirecting: () => <Loading />,
+      })}
+      {...args}
+    />
+  );
+}
