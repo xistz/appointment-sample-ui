2d1fc9d600d9acb548afd15bf008702abb842230
Jonathan Chan <tmxistz@gmail.com>
2020-10-07 18:05:34 +0900

    added index.js for components

---
+++ src/components/index.js
@@ -0,0 +1,19 @@
+import Home from './Home';
+import Header from './Header';
+import Auth0ProviderWithHistory from './Auth0ProviderWithHistory';
+import Profile from './Profile';
+import Register from './Register';
+import PrivateRoute from './PrivateRoute';
+import Loading from './Loading';
+import NotFound from './NotFound';
+
+export {
+  Auth0ProviderWithHistory,
+  Home,
+  Header,
+  Profile,
+  Register,
+  PrivateRoute,
+  Loading,
+  NotFound,
+};
