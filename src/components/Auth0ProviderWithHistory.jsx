01fbe5253804bbc9ccdd8f6c44bc57e7194a9185
Jonathan Chan <tmxistz@gmail.com>
2020-10-09 00:13:31 +0900

    removed debug commit

--- src/components/Auth0ProviderWithHistory.jsx
+++ src/components/Auth0ProviderWithHistory.jsx
@@ -7,10 +7,6 @@ export default function Auth0ProviderWithHistory({ children }) {
   const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
   const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
 
-  console.log(domain);
-  console.log(clientId);
-  console.log(audience);
-
   const history = useHistory();
 
   const onRedirectCallback = (appState) => {
