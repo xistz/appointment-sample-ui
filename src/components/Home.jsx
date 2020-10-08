402fc5d520f49f580455d630003ec9a3e06e6e97
Jonathan Chan <tmxistz@gmail.com>
2020-10-08 14:40:10 +0900

    added route guards for Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import { Redirect } from 'react-router-dom';
 
 import HomeClient from './HomeClient';
 import HomeFinancialPlanner from './HomeFinancialPlanner';
@@ -7,7 +8,13 @@ import HomeFinancialPlanner from './HomeFinancialPlanner';
 export default function Home() {
   const { user } = useAuth0();
 
-  if (user['https://appointment-sample.io/roles'].includes('Client')) {
+  if (user[`${process.env.REACT_APP_AUTH0_NAMESPACE}/roles`].length === 0) {
+    return <Redirect to="/register" />;
+  }
+
+  if (
+    user[`${process.env.REACT_APP_AUTH0_NAMESPACE}/roles`].includes('Client')
+  ) {
     return <HomeClient />;
   }
 
