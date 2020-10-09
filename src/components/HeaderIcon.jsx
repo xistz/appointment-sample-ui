235b26def98854ec983cd8f0f9181bd02b1468e2
Jonathan Chan <tmxistz@gmail.com>
2020-10-09 14:51:22 +0900

    made HeaderIcon a button and redirect to /

--- src/components/HeaderIcon.jsx
+++ src/components/HeaderIcon.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Event as EventIcon } from '@material-ui/icons';
-import { Avatar } from '@material-ui/core';
-import { useAuth0 } from '@auth0/auth0-react';
+import { IconButton } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   eventIcon: {
@@ -12,27 +12,18 @@ const useStyles = makeStyles((theme) => ({
 
 export default function HeaderIcon() {
   const classes = useStyles();
-  const { user, isAuthenticated } = useAuth0();
-
-  if (isAuthenticated) {
-    return (
-      <Avatar
-        alt={user.name}
-        src={user.picture}
-        edge="start"
-        className={classes.eventIcon}
-      >
-        {user.family_name[0] + user.given_name[0]}
-      </Avatar>
-    );
-  }
+  const history = useHistory();
 
   return (
-    <EventIcon
+    <IconButton
       edge="start"
       className={classes.eventIcon}
       color="inherit"
       aria-label="menu"
-    />
+      onClick={() => history.push('/')}
+      s
+    >
+      <EventIcon />
+    </IconButton>
   );
 }
