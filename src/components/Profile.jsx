49e112d069e9f68cb0ff55136b5fe2aaeea0013e
Jonathan Chan <tmxistz@gmail.com>
2020-10-09 15:00:13 +0900

    completed /profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,16 +1,45 @@
 import React from 'react';
+import { Avatar, CssBaseline, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 import { useAuth0 } from '@auth0/auth0-react';
+import LogoutButton from './LogoutButton';
 
-export default function Profile() {
-  const { user, isAuthenticated } = useAuth0();
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+    width: theme.spacing(15),
+    height: theme.spacing(15),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignIn() {
+  const classes = useStyles();
+  const { user } = useAuth0();
 
   return (
-    isAuthenticated && (
-      <div>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar} src={user.picture} />
+        <Typography component="h1" variant="h5">
+          {user.name}
+        </Typography>
+        <Typography component="h1" variant="h5">
+          {user.email}
+        </Typography>
+        <LogoutButton />
       </div>
-    )
+    </Container>
   );
 }
