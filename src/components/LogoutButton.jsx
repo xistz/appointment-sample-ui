75c737273080ecd704515fe0aa47772d3a8bb2ca
Jonathan Chan <tmxistz@gmail.com>
2020-10-09 14:52:50 +0900

    added styles to LogoutButton

--- src/components/LogoutButton.jsx
+++ src/components/LogoutButton.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 export default function LogoutButton() {
+  const classes = useStyles();
   const { logout, isAuthenticated } = useAuth0();
 
   return (
     isAuthenticated && (
-      <Button onClick={() => logout({ returnTo: window.location.origin })}>
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        color="primary"
+        className={classes.submit}
+        onClick={() => logout({ returnTo: window.location.origin })}
+      >
         Log Out
       </Button>
     )
