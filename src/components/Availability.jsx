49a6abb3424cdfcefeea68a002615cb384a4e112
Jonathan Chan <tmxistz@gmail.com>
2020-10-12 22:17:34 +0900

    refactored Availability to use props

--- src/components/Availability.jsx
+++ src/components/Availability.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import {
   Card,
   CardContent,
@@ -8,8 +8,6 @@ import {
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { format, formatISO } from 'date-fns';
-import { useAuth0 } from '@auth0/auth0-react';
-import axios from 'axios';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,55 +17,8 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function Availability({ datetime }) {
-  const [checked, setChecked] = useState(false);
-  const [id, setID] = useState(null);
+export default function Availability({ datetime, id, handleChange }) {
   const classes = useStyles();
-  const { getAccessTokenSilently } = useAuth0();
-
-  useEffect(() => {
-    console.log('call show');
-  }, [datetime]);
-
-  const handleChange = async (event) => {
-    const { checked } = event.target;
-
-    const token = await getAccessTokenSilently();
-    const headers = {
-      Authorization: `Bearer ${token}`,
-    };
-
-    try {
-      if (checked) {
-        console.log('create');
-        const data = {
-          from: formatISO(datetime),
-        };
-
-        const createAvailabilityURL = `${process.env.REACT_APP_API_URL}/availabilities`;
-
-        const response = await axios.post(createAvailabilityURL, data, {
-          headers,
-        });
-
-        const { id } = response.data;
-        setID(id);
-      } else {
-        console.log('delete');
-        const deleteAvailabilityURL = `${process.env.REACT_APP_API_URL}/availabilities/${id}`;
-
-        await axios.delete(deleteAvailabilityURL, { headers });
-
-        setID(null);
-      }
-    } catch (error) {
-      const { message } = error.response.data;
-      console.error(message);
-      return;
-    }
-
-    setChecked(checked);
-  };
 
   return (
     <Card className={classes.root}>
@@ -78,9 +29,13 @@ export default function Availability({ datetime }) {
       </CardContent>
       <FormControlLabel
         control={
-          <Switch checked={checked} onChange={handleChange} name="checkedA" />
+          <Switch
+            checked={id !== undefined}
+            onChange={handleChange}
+            name={formatISO(datetime)}
+          />
         }
-        label={checked ? 'Available' : 'Unavailable'}
+        label={id ? 'Available' : 'Unavailable'}
       />
     </Card>
   );
