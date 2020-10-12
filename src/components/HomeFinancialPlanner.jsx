2a36e1ad127b36b72c48608e09a148d3c2477d2a
Jonathan Chan <tmxistz@gmail.com>
2020-10-13 08:30:21 +0900

    refactored HomeFinancialPlanner to create common Layout

--- src/components/HomeFinancialPlanner.jsx
+++ src/components/HomeFinancialPlanner.jsx
@@ -1,55 +1,18 @@
 import React, { useState } from 'react';
-import { DatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
-import { Typography, Container, Grid, CssBaseline } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import DateFnsUtils from '@date-io/date-fns';
 
+import Layout from './Layout';
 import Availabilities from './Availabilities';
-// import Appointments from './Appointments';
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(4),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-}));
 
 export default function HomeFinancialPlanner() {
   const [date, changeDate] = useState(new Date());
-  const classes = useStyles();
-
-  const disableSunday = (date) => {
-    return date.getDay() === 0;
-  };
 
   return (
-    <Container component="main">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Typography component="h1" variant="h5" align="center" gutterBottom>
-          Please set your availability
-        </Typography>
-        <Grid container spacing={2} justify="center" alignItems="center">
-          <Grid item>
-            <MuiPickersUtilsProvider utils={DateFnsUtils}>
-              <DatePicker
-                autoOk
-                variant="static"
-                openTo="date"
-                value={date}
-                onChange={changeDate}
-                shouldDisableDate={disableSunday}
-              />
-            </MuiPickersUtilsProvider>
-          </Grid>
-          <Grid container item sm wrap="nowrap">
-            <Availabilities date={date} />
-            {/* <Appointments date={date} /> */}
-          </Grid>
-        </Grid>
-      </div>
-    </Container>
+    <Layout
+      date={date}
+      changeDate={changeDate}
+      title="Please set your availability"
+    >
+      <Availabilities date={date} />
+    </Layout>
   );
 }
---
+++ src/components/Layout.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { DatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
+import { Typography, Container, Grid, CssBaseline } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import DateFnsUtils from '@date-io/date-fns';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+}));
+
+export default function Layout({ children, date, changeDate, title }) {
+  const classes = useStyles();
+
+  const disableSunday = (date) => {
+    return date.getDay() === 0;
+  };
+
+  return (
+    <Container component="main">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5" align="center" gutterBottom>
+          {title}
+        </Typography>
+        <Grid container spacing={2} justify="center" alignItems="center">
+          <Grid item>
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <DatePicker
+                autoOk
+                variant="static"
+                openTo="date"
+                value={date}
+                onChange={changeDate}
+                shouldDisableDate={disableSunday}
+              />
+            </MuiPickersUtilsProvider>
+          </Grid>
+          <Grid container item sm wrap="nowrap">
+            {children}
+          </Grid>
+        </Grid>
+      </div>
+    </Container>
+  );
+}
