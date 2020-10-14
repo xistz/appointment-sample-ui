a83539b57dfbcacccd9599f9c883a205f0cd0e86
Jonathan Chan <tmxistz@gmail.com>
2020-10-15 01:06:46 +0900

    centered success messages

--- src/components/AppointmentPickerSuccess.jsx
+++ src/components/AppointmentPickerSuccess.jsx
@@ -4,10 +4,10 @@ import { Typography } from '@material-ui/core';
 export default function AppointmentPickerSuccess() {
   return (
     <React.Fragment>
-      <Typography variant="h5" gutterBottom>
+      <Typography variant="h5" gutterBottom align="center">
         Thank you.
       </Typography>
-      <Typography variant="subtitle1">
+      <Typography variant="subtitle1" align="center">
         The appointment has been made.
       </Typography>
     </React.Fragment>
