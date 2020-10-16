5251ff324fcbe7a677a620ad4d5f112c427d8d45
Jonathan Chan <tmxistz@gmail.com>
2020-10-16 12:00:12 +0900

    Revert "refactored Loading"
    This reverts commit c690996a453a020835267df22382dc1e10d10b4d.

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,20 +1,5 @@
 import React from 'react';
-import { Backdrop, CircularProgress } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
-  backdrop: {
-    zIndex: theme.zIndex.drawer + 1,
-    color: '#fff',
-  },
-}));
-
-export default function SimpleBackdrop() {
-  const classes = useStyles();
-
-  return (
-    <Backdrop className={classes.backdrop} open>
-      <CircularProgress color="inherit" />
-    </Backdrop>
-  );
+export default function Loading() {
+  return <div>Loading...</div>;
 }
