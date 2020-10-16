8fbeaac689dacd629e5712a9ce5cc53f675f5ab8
Jonathan Chan <tmxistz@gmail.com>
2020-10-16 21:52:23 +0900

    added redirect after 5s

--- src/components/AppointmentPickerSuccess.jsx
+++ src/components/AppointmentPickerSuccess.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Typography } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 
 export default function AppointmentPickerSuccess() {
+  const history = useHistory();
+
+  useEffect(() => {
+    const timer = setTimeout(() => history.push('/appointments'), 5000);
+    return () => clearTimeout(timer);
+  }, [history]);
+
   return (
     <React.Fragment>
       <Typography variant="h5" gutterBottom align="center">
         Thank you.
       </Typography>
       <Typography variant="subtitle1" align="center">
-        The appointment has been made.
+        The appointment has been made. You will be redirected in 5s.
       </Typography>
     </React.Fragment>
   );
